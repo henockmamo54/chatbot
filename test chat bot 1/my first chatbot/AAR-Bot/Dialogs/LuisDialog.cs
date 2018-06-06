c231eed6a7bfbda63c45e82bb4561081634589cb
Henock <henockmamo54@gmail.com>
2018-06-06 23:36:42 +0900

    minor change

--- test chat bot 1/my first chatbot/AAR-Bot/Dialogs/LuisDialog.cs
+++ test chat bot 1/my first chatbot/AAR-Bot/Dialogs/LuisDialog.cs
@@ -23,12 +23,15 @@ namespace AAR_Bot.Dialogs
             var value = await result;
             Rootobject obj = await LUISService.GetEntityFromLUIS(value.Text);
 
-            switch (value.Text.ToString()) {
-                case "1": await aboutCourseRegistration.CourseRegistraionOptionSelected(context);  break;
-                case "2": await aboutCourseInfo.CourseInfoOptionSelected(context);  break;
-                case "3": await aboutCredits.CreditsOptionSelected(context);  break;
-                case "4": await aboutOthers.OtherOptionSelected(context);  break;
-                case "5": await aboutHelp.HelpOptionSelected(context);  break;
+            switch (value.Text.ToString())
+            {
+                case "1": await aboutCourseRegistration.CourseRegistraionOptionSelected(context); break;
+                case "2": await aboutCourseInfo.CourseInfoOptionSelected(context); break;
+                case "3": await aboutCredits.CreditsOptionSelected(context); break;
+                case "4": await aboutOthers.OtherOptionSelected(context); break;
+                case "5": await aboutHelp.HelpOptionSelected(context); break;
+                default: { await context.PostAsync("For now the chat bot works only for number inputs. \nWe are working on LUIS Integration. Thank you for your understanding.");
+                        context.Done(obj.topScoringIntent.ToString()); break; }
             }
 
             //context.Done(obj.topScoringIntent.ToString());
