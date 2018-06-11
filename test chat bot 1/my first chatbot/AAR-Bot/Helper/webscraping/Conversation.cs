87c608ed70b0bf08fd2186d85d24c9192342a4c9
Henock <henockmamo54@gmail.com>
2018-06-11 15:52:33 +0900

    directline removed from aar bot

---
+++ test chat bot 1/my first chatbot/AAR-Bot/Helper/webscraping/Conversation.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAR_Bot.Helper.webscraping
+{
+    public class Conversation
+    {
+        public Conversation() { }
+        public Conversation(string conversationId = null, string token = null, int? expiresIn = default(int?), string streamUrl = null, string referenceGrammarId = null, string eTag = null) { }
+
+        [JsonProperty(PropertyName = "conversationId")]
+        public string ConversationId { get; set; }
+        [JsonProperty(PropertyName = "eTag")]
+        public string ETag { get; set; }
+        [JsonProperty(PropertyName = "expires_in")]
+        public int? ExpiresIn { get; set; }
+        [JsonProperty(PropertyName = "referenceGrammarId")]
+        public string ReferenceGrammarId { get; set; }
+        [JsonProperty(PropertyName = "streamUrl")]
+        public string StreamUrl { get; set; }
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
+    }
+}
--- test chat bot 1/my first chatbot/AAR-Bot/Helper/webscraping/ConversationInfo.cs
+++ test chat bot 1/my first chatbot/AAR-Bot/Helper/webscraping/ConversationInfo.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.Bot.Connector.DirectLine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace AAR_Bot.Helper.webscraping
 {
