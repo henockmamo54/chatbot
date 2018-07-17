b7400013eef41480439f857a67800856aeb93994
Henock <henockmamo54@gmail.com>
2018-07-17 13:12:09 +0900

    Periodic token refreshing added

--- test chat bot 1/my first chatbot/AAR-Bot/Global.asax.cs
+++ test chat bot 1/my first chatbot/AAR-Bot/Global.asax.cs
@@ -5,8 +5,12 @@ using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -15,30 +19,65 @@ namespace AAR_Bot
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        CancellationTokenSource _getTokenAsyncCancellation = new CancellationTokenSource();
+
         protected void Application_Start()
         {
+            Conversation.UpdateContainer(
+                builder =>
+                {
+                    builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+
+                    // Bot Storage: register state storage for your bot
+                    // Default store: volatile in-memory store - Only for prototyping!
+                    // var store = new InMemoryDataStore();
+
+                    var uri = new Uri("https://mjukje.documents.azure.com:443/");
+                    var key = "5MfRHpG8g1x534IXnilPh4DzhZOk94S440mTZ4rCSTiUFnyzq1Fr4lnOi9IscugVUNNFVFWahEQs4Yo06AqQDA==";
+
+                    var store = new DocumentDbBotDataStore(uri, key);
+
+                    builder.Register(c => store)
+                        .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                        .AsSelf()
+                        .SingleInstance();
+                });
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            Conversation.UpdateContainer(
-    builder =>
-    {
-        builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+            //============================================ for optimization ============================================
+            //==========================================================================================================
 
-        // Bot Storage: register state storage for your bot
-        // Default store: volatile in-memory store - Only for prototyping!
-        // var store = new InMemoryDataStore();
+            
+            var appID = ConfigurationManager.AppSettings["MicrosoftAppId"];
+            var appPassword = ConfigurationManager.AppSettings["MicrosoftAppPassword"];
+            if (!string.IsNullOrEmpty(appID) && !string.IsNullOrEmpty(appPassword))
+            {
+                var credentials = new MicrosoftAppCredentials(appID, appPassword);
+                Task.Factory.StartNew(async () =>
+                {
+                    while (!_getTokenAsyncCancellation.IsCancellationRequested)
+                    {
+                        try
+                        {
+                            var token = await credentials.GetTokenAsync().ConfigureAwait(false);
+                        }
+                        catch (MicrosoftAppCredentials.OAuthException ex)
+                        {
+                            Trace.TraceError(ex.Message);
+                        }
+                        await Task.Delay(TimeSpan.FromMinutes(30), _getTokenAsyncCancellation.Token).ConfigureAwait(false);
+                    }
+                }).ConfigureAwait(false);
+            }
 
-        var uri = new Uri("https://mjukje.documents.azure.com:443/");
-        var key = "5MfRHpG8g1x534IXnilPh4DzhZOk94S440mTZ4rCSTiUFnyzq1Fr4lnOi9IscugVUNNFVFWahEQs4Yo06AqQDA==";
 
-        var store = new DocumentDbBotDataStore(uri, key);
 
-        builder.Register(c => store)
-            .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
-            .AsSelf()
-            .SingleInstance();
-    });
+        }
 
+        protected void Application_End()
+        {
+            _getTokenAsyncCancellation.Cancel();
         }
     }
 }
