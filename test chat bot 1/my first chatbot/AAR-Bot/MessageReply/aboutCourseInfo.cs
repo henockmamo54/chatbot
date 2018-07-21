d49bc9a53ba49e17de4e9d9d74c9387b59997882
Henock <henockmamo54@gmail.com>
2018-07-22 01:44:54 +0900

    list option modified

--- test chat bot 1/my first chatbot/AAR-Bot/MessageReply/aboutCourseInfo.cs
+++ test chat bot 1/my first chatbot/AAR-Bot/MessageReply/aboutCourseInfo.cs
@@ -3,6 +3,7 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System;
 
 namespace AAR_Bot.MessageReply
 {
@@ -10,27 +11,14 @@ namespace AAR_Bot.MessageReply
     {
         public static async Task CourseInfoOptionSelected(IDialogContext context)
         {
-            if (context.Activity.ChannelId == "emulator")
+            if (context.Activity.ChannelId == "facebook")
             {
-                var activity = context.MakeMessage();
-                activity.Text = RootDialog._storedvalues._courseInfoSelected.Replace("\n", "\n\n ");
-                activity.SuggestedActions = new SuggestedActions()
-                {
-                    Actions = new List<CardAction>()
-                    {
-                        new CardAction(){ Title = RootDialog._storedvalues._openedMajorCourses, Type=ActionTypes.ImBack, Value="Opened LiberalArts" },
-                        new CardAction(){ Title = RootDialog._storedvalues._openedLiberalArts, Type=ActionTypes.ImBack, Value="Opened Major" },
-                        new CardAction(){ Title = RootDialog._storedvalues._syllabus, Type=ActionTypes.ImBack, Value="Syllabus" },
-                        new CardAction(){ Title = RootDialog._storedvalues._lecturerInfo, Type=ActionTypes.ImBack, Value="Lecture Info" },
-                        new CardAction(){ Title = RootDialog._storedvalues._mandatorySubject, Type=ActionTypes.ImBack, Value="Mandatory Subject" },
-                        new CardAction(){ Title = RootDialog._storedvalues._prerequisite, Type=ActionTypes.ImBack, Value="Prerequisite" },
-                        new CardAction(){ Title = RootDialog._storedvalues._gotostart, Type=ActionTypes.ImBack, Value="Go To Start" },
-                        new CardAction(){ Title = "Help", Type=ActionTypes.ImBack, Value="Help" }
-                    }
-                };
+                var reply = context.MakeMessage();
+                reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                reply.Attachments = GetCardsAttachments();
+                await context.PostAsync(reply);
 
-                await context.PostAsync(activity);
-                context.Wait(HandleCourseInfoOptionSelection);
+                await showOptions(context);
             }
             else
             {
@@ -45,6 +33,30 @@ namespace AAR_Bot.MessageReply
             }
 
         }
+
+
+        public static async Task showOptions(IDialogContext context) {
+            var activity = context.MakeMessage();
+            activity.Text = RootDialog._storedvalues._courseInfoSelected.Replace("\n", "\n\n ");
+            activity.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                    {
+                        new CardAction(){ Title = RootDialog._storedvalues._openedMajorCourses, Type=ActionTypes.ImBack, Value="Opened LiberalArts" },
+                        new CardAction(){ Title = RootDialog._storedvalues._openedLiberalArts, Type=ActionTypes.ImBack, Value="Opened Major" },
+                        new CardAction(){ Title = RootDialog._storedvalues._syllabus, Type=ActionTypes.ImBack, Value="Syllabus" },
+                        new CardAction(){ Title = RootDialog._storedvalues._lecturerInfo, Type=ActionTypes.ImBack, Value="Lecture Info" },
+                        new CardAction(){ Title = RootDialog._storedvalues._mandatorySubject, Type=ActionTypes.ImBack, Value="Mandatory Subject" },
+                        new CardAction(){ Title = RootDialog._storedvalues._prerequisite, Type=ActionTypes.ImBack, Value="Prerequisite" },
+                        new CardAction(){ Title = RootDialog._storedvalues._gotostart, Type=ActionTypes.ImBack, Value="Go To Start" },
+                        new CardAction(){ Title = "Help", Type=ActionTypes.ImBack, Value="Help" }
+                    }
+            };
+
+            await context.PostAsync(activity);
+            context.Wait(HandleCourseInfoOptionSelection);
+        }
+
         public static async Task HandleCourseInfoOptionSelection(IDialogContext context, IAwaitable<string> result)
         {
             var value = await result;
@@ -76,7 +88,6 @@ namespace AAR_Bot.MessageReply
             string value = myresult.Text;
 
             if (value.ToString() == RootDialog._storedvalues._gotostart) await RootDialog.ShowWelcomeOptions(context);
-
             else if (value.ToString() == RootDialog._storedvalues._help) await aboutHelp.HelpOptionSelected(context);
 
             else
@@ -91,10 +102,91 @@ namespace AAR_Bot.MessageReply
                 else if (value.ToString() == RootDialog._storedvalues._gotostart) await RootDialog.ShowWelcomeOptions(context);
 
                 //await RootDialog.ShowWelcomeOptions(context);           //Return To Start
-                await CourseInfoOptionSelected(context);
+                //await CourseInfoOptionSelected(context);
+                await showOptions(context);
             }
         }
 
+
+
+        private static IList<Attachment> GetCardsAttachments()
+        {
+            var openedMajorCourses = new HeroCard
+            {
+                Title = "이번학기 전공개설강의",
+                Subtitle = "이번학기 전공개설강의",          //Location of information in MJU homepage
+                Text = "이번학기 전공개설강의정보",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "https://drive.google.com/open?id=1iVNvUHc2-Qs_AXWGgnXpsPx3mp0BWCK7") }
+            };
+
+            var openedLiberalArts = new HeroCard
+            {
+                Title = "이번학기 교양개설강의",
+                Subtitle = "이번학기 교양개설강의",          //Location of information in MJU homepage
+                Text = "이번학기 교양개설강의정보",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "https://drive.google.com/open?id=1Q7Ej1JB2OHcBP-TjXEdZYWz8H7ncUtpd") }
+            };
+
+            var syllabus = new HeroCard
+            {
+                Title = "강의계획서",
+                Subtitle = "강의별 강의계획서",
+                Text = "강의계획서 열람 정보입니다.",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "https://drive.google.com/open?id=1Yn5FBeBVedQdodPsnM3I_1kWcKyL2abM") }
+            };
+
+            var lecturerInfo = new HeroCard
+            {
+                Title = "교수 정보",
+                Subtitle = "교수 홈페이지 검색",
+                Text = "교수 정보",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "http://home.mju.ac.kr/mainIndex/searchHomepage.action") }
+            };
+
+            var mandatorySubject = new HeroCard
+            {
+                Title = "정보통신공학과 선후수 과목정보",
+                Subtitle = "정보통신공학과 선후수 과목정보",
+                Text = "정보통신공학과 선후수 과목정보",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "https://drive.google.com/open?id=1Fy7bAxihUXqlNLLToimYcKSiTHg_XdGe") }
+            };
+
+            var prerequisite = new HeroCard
+            {
+                Title = "정보통신공학과 선후수 과목정보",
+                Subtitle = "정보통신공학과 선후수 과목정보",
+                Text = "정보통신공학과 선후수 과목정보",
+                //Images = new List<CardImage> { new CardImage("http://www.kimaworld.net/data/file/char/3076632059_6ySVa5o9_EBAA85ECA7801.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl,
+                                                RootDialog._storedvalues._goToButton,
+                                                value: "http://www.mju.ac.kr/mbs/mjukr/images/editor/1406095802964_img_2017.jpg") }
+            };
+
+            return new List<Attachment>()
+            {
+                openedMajorCourses.ToAttachment(),
+                openedLiberalArts.ToAttachment(),
+                syllabus.ToAttachment(),
+                mandatorySubject.ToAttachment(),
+                prerequisite.ToAttachment()
+            };
+
+            }
 
 
         //================================================================================================================================================
