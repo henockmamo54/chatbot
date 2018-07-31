707f3e8dc9e14b20ce2ee1f2c29f2a6a3828815a
Henock <henockmamo54@gmail.com>
2018-08-01 00:44:31 +0900

    we scraping second tab added

--- test chat bot 1/my first chatbot/webscraping/webscraping/Program.cs
+++ test chat bot 1/my first chatbot/webscraping/webscraping/Program.cs
@@ -18,15 +18,13 @@ namespace webscraping
         public static List<FoodMenu> weeklyFoodMenu = new List<FoodMenu>();
         static void Main(string[] args)
         {
-            var x= getRestaurantMenu();
+            var x = getRestaurant2Menu();
             x.Wait();
 
-            var y=getLibraryInfo();
+            var y = getLibraryInfo();
             y.Wait();
-            
+
         }
-
-
         public static async Task getRestaurantMenu()
         {
             var url = "http://www.mju.ac.kr/mbs/mjukr/jsp/restaurant/restaurant.jsp?configIdx=36337&id=mjukr_051002050000";
@@ -84,6 +82,70 @@ namespace webscraping
             {
                 myList = JsonConvert.SerializeObject(foodmenuforjsonobj),
                 id = "FoodMenu",
+                timestamp = DateTimeOffset.Now,
+                watermark = ""
+            };
+
+            await myDbService.SetInfoAsync(conversationinfo, "FoodMenu");
+
+            Console.WriteLine();
+        }
+
+        public static async Task getRestaurant2Menu()
+        {
+            var url = "http://www.mju.ac.kr/mbs/mjukr/jsp/restaurant/restaurant.jsp?configIdx=36548&id=mjukr_051002020000";
+
+            var httpclient = new HttpClient();
+            var html = await httpclient.GetStringAsync(url);
+
+
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var cafeteria_container = htmlDoc.DocumentNode.Descendants("table")
+                .Where(node => node.GetAttributeValue("summary", "")
+                .Equals("학생회관 식당 ")).ToList();
+
+            //var menu_table = cafeteria_container[0].Descendants("table").
+            //    Where(node => node.GetAttributeValue("summary", "")
+            //    .Contains("식단내용")).ToList();
+
+            weeklyFoodMenu = new List<FoodMenu>();
+            //foreach (var menu in menu_table) {
+            //for (int i = 0; i < menu_table.Count; i++)
+            //{
+            //var menu = menu_table[i];
+            var menu = cafeteria_container[0];
+            FoodMenu todaysMenu = new FoodMenu();
+
+            var date = menu.Descendants("th").FirstOrDefault().InnerText;
+            todaysMenu.Date = date.Trim();
+
+            var menudetails = menu.Descendants("tr").ToList();
+            foreach (var menudetail in menudetails)
+            {
+                try
+                {
+                    var menudetail_subOption = menudetail.Descendants("td").ToList()[0].InnerText;
+                    var menudetail_headerOption = "http://www.mju.ac.kr/" +menudetail.Descendants("th").ToList()[0].Descendants("img").FirstOrDefault().Attributes["src"].Value;
+
+                   string[] actualvalueswithsubmenu = { menudetail_subOption.Trim(), menudetail_headerOption.Trim() };
+                    todaysMenu.Details.Add(actualvalueswithsubmenu);
+                }
+                catch (Exception ee) { }
+            }
+
+            weeklyFoodMenu.Add(todaysMenu);
+
+            //}
+
+            FoodMenuListModel foodmenuforjsonobj = new FoodMenuListModel();
+            foodmenuforjsonobj.foodMenus = weeklyFoodMenu;
+            ConversationInfo conversationinfo = new ConversationInfo
+            {
+                myList = JsonConvert.SerializeObject(foodmenuforjsonobj),
+                id = "FoodMenu2",
                 timestamp = DateTimeOffset.Now,
                 watermark = ""
             };
