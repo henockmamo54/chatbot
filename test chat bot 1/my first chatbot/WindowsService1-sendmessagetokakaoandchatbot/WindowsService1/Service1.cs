57edd1981a521d7543c107e80d705aa6700df700
Henock <henockmamo54@gmail.com>
2018-08-09 17:33:15 +0900

    clean up

--- test chat bot 1/my first chatbot/WindowsService1-sendmessagetokakaoandchatbot/WindowsService1/Service1.cs
+++ test chat bot 1/my first chatbot/WindowsService1-sendmessagetokakaoandchatbot/WindowsService1/Service1.cs
@@ -31,7 +31,7 @@ namespace WindowsService1
 
         }
 
-       
+
         private async void timer_elasped()
         {
             Activity usermessage = new Activity
@@ -41,8 +41,10 @@ namespace WindowsService1
                 Type = ActivityTypes.Message,
                 TextFormat = "plain"
             };
+            //chat bot service
             await client.Conversations.PostActivityAsync(this.conversation.ConversationId, usermessage);
 
+            //kakao web service
             var url = "https://mjukjeweb.azurewebsites.net/keyboard";
             var httpclient = new HttpClient();
             var html = await httpclient.GetStringAsync(url);
@@ -50,13 +52,8 @@ namespace WindowsService1
             using (StreamWriter writer =
             new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
             {
-                //writer.WriteLine("Important data line 1");
-                //writer.WriteLine("Line 2  ==== > Datasend");
-                writer.WriteLine("Data Send: "+DateTime.Now.ToString());
-                //writer.WriteLine("key board API");
-                //writer.WriteLine(html.ToString());
+                writer.WriteLine("Data Send: " + DateTime.Now.ToString());
             }
-            
 
         }
 
@@ -68,80 +65,13 @@ namespace WindowsService1
         {
             client = new DirectLineClient(directlinesecrte);
             conversation = client.Conversations.StartConversation();
-            new System.Threading.Thread(async () => await ReadBotMessageAsync(client, conversation.ConversationId)).Start();
         }
 
-
-        private async Task ReadBotMessageAsync(DirectLineClient client, string conversationId)
-        {
-            string watermark = null;
-            while (true)
-            {
-
-                var activityset = await client.Conversations.GetActivitiesAsync(conversationId, watermark);
-                watermark = activityset.Watermark;
-
-                var activities = from x in activityset.Activities where x.From.Id == botID select x;
-
-                foreach (Activity activity in activities)
-                {
-                    if (activity.Text != null)
-                    {
-                        string message = activity.Text;
-                        //System.Diagnostics.Debug.WriteLine(message);
-
-                        //using (StreamWriter writer =
-                        //new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
-                        //{
-                        //    writer.WriteLine("======= data received");
-                        //}
-                        //// Append line to the file.
-                        //using (StreamWriter writer =
-                        //    new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
-                        //{
-                        //    writer.WriteLine(message);
-                        //    writer.WriteLine(DateTime.Now.ToString());
-                        //}
-
-
-                    }
-                }
-
-            }
-        }
 
 
         protected override void OnStart(string[] args)
         {
             InitClient();
-            
-            //try
-            //{
-            //    using (StreamWriter writer =
-            //new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
-            //    {
-            //        writer.WriteLine("Important data line 1");
-            //    }
-            //    // Append line to the file.
-            //    using (StreamWriter writer =
-            //        new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
-            //    {
-            //        writer.WriteLine("Line 2");
-            //        writer.WriteLine(DateTime.Now.ToString());
-            //    }
-
-            //}
-            //catch (Exception ex)
-            //{
-            //    // Append line to the file.
-            //    using (StreamWriter writer =
-            //        new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "onStart.txt", true))
-            //    {
-            //        writer.WriteLine("errror");
-            //        writer.WriteLine(ex.Message);
-            //        writer.WriteLine(DateTime.Now.ToString());
-            //    }
-            //}
 
             thread = new Thread(this.DoWork);
             thread.Start();
@@ -152,7 +82,6 @@ namespace WindowsService1
             while (true)
             {
                 timer_elasped();
-                //Thread.Sleep(1500000);
                 Thread.Sleep(900000);
             }
         }
